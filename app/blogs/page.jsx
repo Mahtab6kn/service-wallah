e857f3fa1c2f56458c975ada97a40f1182764abe
Mahtab6kn <mahtab6kn@gmail.com>
2024-10-03 12:54:02 +0530

    fix the issue in blogs page

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,4 +1,5 @@
 import BlogCard from "@/components/admin/blogs/BlogCard";
+import Loading from "@/components/Loading";
 import PaginationBtn from "@/components/PaginationBtn";
 import React, { Suspense } from "react";
 
@@ -27,22 +28,20 @@ async function getBlogs(page = 1, limit = 9) {
 }
 const page = async ({ searchParams: { page, size } }) => {
   const blogs = await getBlogs(page, size);
-
+  if (!blogs) {
+    return <Loading />;
+  }
   return (
     <main className="relative p-4 sm:p-8 bg-gray-50">
       <h2 className="text-2xl lg:text-4xl font-bold text-center text-teal-500 font-aclonica leading-tight mb-4 sm:mb-8">
         All Blogs
       </h2>
 
-      {blogs.data ? (
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 place-items-center md:place-items-start px-2 sm:px-5 ">
-          {blogs.data.map((blog) => (
-            <BlogCard key={blog._id} blog={blog} page="user" />
-          ))}
-        </div>
-      ) : (
-        <div className="text-center">Blogs Not Found</div>
-      )}
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 place-items-center md:place-items-start px-2 sm:px-5 ">
+        {blogs.data.map((blog) => (
+          <BlogCard key={blog._id} blog={blog} page="user" />
+        ))}
+      </div>
       <div className="mt-3">
         <PaginationBtn totalPages={blogs.pagination?.totalPages} />
       </div>
