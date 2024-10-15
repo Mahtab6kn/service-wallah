71f9c2800ecfb746a74022a036da14c886b3f30f
Musharraf Jamal <musharrafjamal08@gmail.com>
2024-10-15 14:55:14 +0530

    update git commit error

--- app/api/send-notification/route.js
+++ app/api/send-notification/route.js
@@ -15,20 +15,6 @@ const firebaseConfig = {
   universeDomain: process.env.NEXT_PUBLIC_FIREBASE_UNIVERSE_DOMAIN,
 };
 
-const firebaseConfig = {
-  type: "service_account",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  privateKeyId: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
-  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-  clientId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
-  authUri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI,
-  tokenUri: process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI,
-  authProviderCertUrl: process.env.NEXT_PUBLIC_FIREBASE_AUTH_PROVIDER_CERT_URL,
-  clientCertUrl: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_CERT_URL,
-  universeDomain: process.env.NEXT_PUBLIC_FIREBASE_UNIVERSE_DOMAIN,
-};
-
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
   admin.initializeApp({
