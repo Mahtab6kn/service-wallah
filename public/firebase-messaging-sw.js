d07a751c83f1eb14b22a7ed4f82e58c988e2a09e
Musharraf Jamal <musharrafjamal08@gmail.com>
2024-10-17 13:21:31 +0530

    update firebase messaging page

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,9 +3,15 @@ importScripts(
   "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
 
-import firebaseConfig from "@/firebase-config";
-
-console.log({firebaseConfig})
+const firebaseConfig = {
+  apiKey: "AIzaSyDB6LO5AUsir9MQLnUavkhF13y_koKjGUc",
+  authDomain: "service-wallah-1ed8c.firebaseapp.com",
+  projectId: "service-wallah-1ed8c",
+  storageBucket: "service-wallah-1ed8c.appspot.com",
+  messagingSenderId: "239037401868",
+  appId: "1:239037401868:web:243caa96bd6234fa5f1729",
+  measurementId: "G-BJR3FR8EYE",
+};
 
 firebase.initializeApp(firebaseConfig);
 
